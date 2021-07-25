49349094267338b0878ea53e95cf8b69437c6abd
shreesh <tanaykumar103@gmail.com>
2021-07-25 18:17:24 +0530

    updated configs

--- configs/bspwm/autostart.sh
+++ configs/bspwm/autostart.sh
@@ -21,7 +21,7 @@ $HOME/.config/polybar/launch.sh &
 
 #Some ways to set your wallpaper besides variety or nitrogen
 #feh --bg-scale ~/.config/bspwm/wall.png &
-#feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
+feh --bg-fill /home/shreesh/Pictures/wallpapers/XFCE4.png &
 #feh --randomize --bg-fill ~/Képek/*
 #feh --randomize --bg-fill ~/Dropbox/Apps/Desktoppr/*
 
@@ -30,7 +30,7 @@ xsetroot -cursor_name left_ptr &
 run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 
 #conky -c $HOME/.config/bspwm/system-overview &
-run variety &
+#run variety &
 run nm-applet &
 run pamac-tray &
 run xfce4-power-manager &
@@ -40,7 +40,7 @@ picom --config $HOME/.config/bspwm/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 run volumeicon &
-#nitrogen --restore &
+nitrogen --restore &
 #run caffeine &
 #run vivaldi-stable &
 #run firefox &
